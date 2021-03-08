e3e871458ed1c9ea95e548f77a929520ced5d357
Coppelia <mfreese@gmx.ch>
2021-03-08 14:21:13 +0100

    initial commit

---
+++ simAddOnCustom data block explorer.lua
@@ -0,0 +1,103 @@
+function sysCall_info()
+    return {autoStart=false}
+end
+
+function sysCall_init()
+    sim.addLog(sim.verbosity_scriptinfos,"This tool will display the custom data blocks attached to the selected object, or the custom data blocks attached to the scene, if no object is selected. Custom data blocks can be written and read with simWriteCustomDataBlock and simReadCustomDataBlock.")
+    object=-1
+end
+
+function sysCall_addOnScriptSuspend()
+    return {cmd='cleanup'}
+end
+
+function sysCall_cleanup()
+    hideDlg()
+end
+
+function sysCall_beforeSimulation()
+    hideDlg()
+end
+
+function sysCall_beforeInstanceSwitch()
+    hideDlg()
+end
+
+function clearClick_callback(ui,id,newVal)
+    sim.writeCustomDataBlock(object,tags[id],'')
+    hideDlg()
+end
+
+function onCloseClicked()
+    leaveNow=true
+end
+
+function showDlg()
+    if not ui then
+        local pos='position="-50,50" placement="relative"'
+        if uiPos then
+            pos='position="'..uiPos[1]..','..uiPos[2]..'" placement="absolute"'
+        end
+        local title="Custom data blocks in scene:"
+        if object>=0 then
+            title="Custom data blocks in object '"..sim.getObjectName(object).."':"
+        end
+        if not ui then
+            xml = '<ui title="Custom Data Block Reader" closeable="true" on-close="onCloseClicked" resizable="false" '..pos..'>'
+            xml=xml..'<label text="'..title..'" style="* {margin-right: 100px;}"/>'
+            xml=xml..'<group layout="form" flat="true">'
+            for i=1,#tags,1 do
+                xml=xml..'<label text="'..tags[i]..'  ('..sizes[i]..' bytes)"/>'
+                xml=xml..'<button text="Clear" checked="false" on-click="clearClick_callback" id="'..i..'" />'
+            end
+            xml=xml..'</group></ui>'
+
+            ui=simUI.create(xml)
+        end
+    end
+end
+
+function hideDlg()
+    if ui then
+        uiPos={}
+        uiPos[1],uiPos[2]=simUI.getPosition(ui)
+        simUI.destroy(ui)
+        ui=nil
+    end
+end
+
+function sysCall_nonSimulation()
+    if leaveNow then
+        return {cmd='cleanup'}
+    end
+    local s=sim.getObjectSelection()
+    tags=nil
+    sizes=nil
+    local previousObject=object
+    object=-1
+    if s then
+        if #s>=1 then
+            if s[#s]>=0 then
+                object=s[#s]
+                tags=sim.readCustomDataBlockTags(object)
+            end
+        end
+    else
+        tags=sim.readCustomDataBlockTags(sim.handle_scene)
+    end
+    if previousObject~=object then
+        hideDlg()
+    end
+    if tags then
+        while #tags>10 do
+            table.remove(tags,#tags)
+        end
+        sizes={}
+        for i=1,#tags,1 do
+            sizes[i]=#sim.readCustomDataBlock(object,tags[i])
+        end
+        showDlg()
+    else
+        hideDlg()
+    end
+end
---
+++ simAddOnName change.lua
@@ -0,0 +1,93 @@
+function sysCall_info()
+    return {autoStart=false}
+end
+
+function sysCall_addOnScriptSuspend()
+    return {cmd='cleanup'}
+end
+
+function sysCall_init()
+    sim.addLog(sim.verbosity_scriptinfos,"This tool allows to replace/change names of selected objects.")
+    strings={"originalString","replacementString"}
+end
+
+function sysCall_nonSimulation()
+    if leaveNow then
+        return {cmd='cleanup'}
+    end
+    local selectedObjects=sim.getObjectSelection()
+    if selectedObjects and (#selectedObjects>=1) then
+        showDlg()
+    else
+        hideDlg()
+    end
+end
+
+function sysCall_beforeSimulation()
+    hideDlg()
+end
+
+function sysCall_cleanup()
+    hideDlg()
+end
+
+function sysCall_beforeInstanceSwitch()
+    hideDlg()
+end
+
+function showDlg()
+    if not ui then
+        local pos='position="-50,50" placement="relative"'
+        if uiPos then
+            pos='position="'..uiPos[1]..','..uiPos[2]..'" placement="absolute"'
+        end
+        local xml ='<ui title="Name change tool" activate="false" closeable="true" on-close="close_callback" '..pos..[[>
+            <group layout="form" flat="true">
+            <label text="replace occurences of"/>
+            <edit on-editing-finished="editFinished_callback" id="1" />
+            <label text="with string"/>
+            <edit on-editing-finished="editFinished_callback" id="2" />
+            </group>
+            <button text="perform operation on selected objects" on-click="replace_callback" id="3"/>
+        </ui>]]
+        ui=simUI.create(xml)
+        simUI.setEditValue(ui,1,strings[1])
+        simUI.setEditValue(ui,2,strings[2])
+    end
+end
+
+function hideDlg()
+    if ui then
+        uiPos={}
+        uiPos[1],uiPos[2]=simUI.getPosition(ui)
+        simUI.destroy(ui)
+        ui=nil
+    end
+end
+
+function editFinished_callback(ui,id,v)
+    strings[id]=v
+end
+
+function replace_callback(ui,id,v)
+    local selectedObjects=sim.getObjectSelection()
+    if #strings[1]>0 then
+        local err=false
+        for i=1,#selectedObjects,1 do
+            local name=sim.getObjectName(selectedObjects[i])
+            local newName,r=string.gsub(name,strings[1],strings[2])
+            if (r>0) then
+                if sim.setObjectName(selectedObjects[i]|sim.handleflag_silenterror,newName)<0 then
+                    err=true
+                end
+            end
+        end
+        if err then
+            sim.msgBox(sim.msgbox_type_warning,sim.msgbox_buttons_ok,"Name change","Some objects could not be renamed. Make sure the resulting names are valid scene object names.")
+        end
+    end
+end
+
+function close_callback()
+    leaveNow=true
+end
