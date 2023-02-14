105d5a5f8d4614cb73d2b21e30a5df2b03b85fad
Federico Ferri <federico.ferri.it@gmail.com>
2023-02-14 13:27:25 +0100

    add two simple dev addons

---
+++ simAddOnEvent viewer.lua
@@ -0,0 +1,24 @@
+function sysCall_info()
+    return {autoStart=false,menu='Developer tools\nEvent viewer'}
+end
+
+function sysCall_init()
+    cbor=require'org.conman.cbor'
+    sim.test('sim.enableEvents',true)
+    sim.test('sim.mergeEvents',true)
+    sim.test('sim.cborEvents',true)
+    consoleHandle=sim.auxiliaryConsoleOpen('Event viewer',500,16)
+end
+
+function sysCall_addOnScriptSuspend()
+    return {cmd='cleanup'}
+end
+
+function sysCall_cleanup()
+    sim.auxiliaryConsoleClose(consoleHandle)
+end
+
+function sysCall_event(event)
+    event=cbor.decode(event)
+    sim.auxiliaryConsolePrint(consoleHandle,_S.tableToString(event,{},99)..'\n')
+end
---
+++ simAddOnMessage viewer.lua
@@ -0,0 +1,19 @@
+function sysCall_info()
+    return {autoStart=false,menu='Developer tools\nMessage viewer'}
+end
+
+function sysCall_init()
+    consoleHandle=sim.auxiliaryConsoleOpen('Broadcast message viewer',500,16)
+end
+
+function sysCall_addOnScriptSuspend()
+    return {cmd='cleanup'}
+end
+
+function sysCall_cleanup()
+    sim.auxiliaryConsoleClose(consoleHandle)
+end
+
+function sysCall_msg(event)
+    sim.auxiliaryConsolePrint(consoleHandle,_S.tableToString(event,{},99)..'\n')
+end
