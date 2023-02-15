2f387c6992022ca6ab2e0a919a73ca44df956e90
Federico Ferri <federico.ferri.it@gmail.com>
2023-02-15 10:43:47 +0100

    moved code to simQHull.computeShape

--- simAddOnMesh convex hull.lua
+++ simAddOnMesh convex hull.lua
@@ -5,27 +5,10 @@ end
 function sysCall_init()
     if sim.isPluginLoaded('QHull') then
         local sel=sim.getObjectSelection()
-        local numShapesFound=0
-        local vert={}
-        for _,h in ipairs(sel) do
-            local t=sim.getObjectType(h)
-            if t==sim.object_shape_type then
-                local v,i,n=sim.getShapeMesh(h)
-                local m=sim.getObjectMatrix(h,-1)
-                v=sim.multiplyVector(m,v)
-                for _,x in ipairs(v) do table.insert(vert,x) end
-                numShapesFound=numShapesFound+1
-            elseif t==sim.object_dummy_type then
-                local p=sim.getObjectPosition(h,-1)
-                for _,x in ipairs(p) do table.insert(vert,x) end
-                numShapesFound=numShapesFound+1
-            end
-        end
-        if numShapesFound==0 then
-            simUI.msgBox(simUI.msgbox_type.critical,simUI.msgbox_buttons.ok,'Mesh convex hull add-on','Select at least one shape.')
+        local ok,err=pcall(simQHull.computeShape,table.unpack(sel))
+        if not ok then
+            simUI.msgBox(simUI.msgbox_type.critical,simUI.msgbox_buttons.ok,'Mesh convex hull add-on','simQHull error: '..err)
         else
-            local v,i=simQHull.compute(vert,true)
-            local h=sim.createMeshShape(3,math.pi/8,v,i)
             sim.announceSceneContentChange()
         end
     else
