e7612a3c88f65ddf61a801bc7f8622af2c389797
Coppelia <mfreese@gmx.ch>
2023-03-27 13:36:43 +0200

    fixed DH add-ons

--- simAddOnDenavit Hartenberg creator.lua
+++ simAddOnDenavit Hartenberg creator.lua
@@ -10,7 +10,7 @@ function sysCall_init()
     sim.addLog(sim.verbosity_scriptinfos,"This tool allows to create joints with the Denavit-Hartenberg notation. Simply select an object in the scene on top of which you wish to create a joint, then adjust the D-H parameters in the dialog.")
     d=0.05
     theta=math.pi/2
-    a=0.1
+    r=0.1
     alpha=math.pi/4
 end
 
@@ -21,13 +21,13 @@ function showDlg()
             pos='position="'..uiPos[1]..','..uiPos[2]..'" placement="absolute"'
         end
         local xml ='<ui title="DH joint creator" activate="false" on-close="close_callback" closeable="true" layout="form" '..pos..[[>
-                <label text="d"/>
+                <label text="d [m]"/>
                 <edit value="" id="1" on-editing-finished="d_callback"/>
-                <label text="theta"/>
+                <label text="theta [deg]"/>
                 <edit value="" id="2" on-editing-finished="theta_callback"/>
-                <label text="a"/>
-                <edit value="" id="3" on-editing-finished="a_callback"/>
-                <label text="alpha"/>
+                <label text="r [m]"/>
+                <edit value="" id="3" on-editing-finished="r_callback"/>
+                <label text="alpha [deg]"/>
                 <edit value="" id="4" on-editing-finished="alpha_callback"/>
                 <button text="Create revolute joint" on-click="rev_callback"/>
                 <button text="Create prismatic joint" on-click="prism_callback"/>
@@ -35,7 +35,7 @@ function showDlg()
         ui=simUI.create(xml)
         simUI.setEditValue(ui,1,string.format("%.4f",d))
         simUI.setEditValue(ui,2,string.format("%.1f",theta*180/math.pi))
-        simUI.setEditValue(ui,3,string.format("%.4f",a))
+        simUI.setEditValue(ui,3,string.format("%.4f",r))
         simUI.setEditValue(ui,4,string.format("%.1f",alpha*180/math.pi))
     end
 end
@@ -55,11 +55,10 @@ end
 
 function d_callback(ui,id,v)
     v=tonumber(v)
-    if v then
-        if v<0 then v=0 end
-        if v>1 then v=1 end
-        d=v
+    if not v then
+        v=0
     end
+    d=v
     simUI.setEditValue(ui,1,string.format("%.4f",d))
 end
 
@@ -68,19 +67,22 @@ function theta_callback(ui,id,v)
     if v then
         if v<-180 then v=-180 end
         if v>180 then v=180 end
-        theta=v*math.pi/180
+    else
+        v=0
     end
+    theta=v*math.pi/180
     simUI.setEditValue(ui,2,string.format("%.1f",theta*180/math.pi))
  end
 
-function a_callback(ui,id,v)
+function r_callback(ui,id,v)
     v=tonumber(v)
     if v then
         if v<0 then v=0 end
-        if v>1 then v=1 end
-        a=v
+    else
+        v=0
     end
-    simUI.setEditValue(ui,3,string.format("%.4f",a))
+    r=v
+    simUI.setEditValue(ui,3,string.format("%.4f",r))
 end
 
 function alpha_callback(ui,id,v)
@@ -88,8 +90,10 @@ function alpha_callback(ui,id,v)
     if v then
         if v<-180 then v=-180 end
         if v>180 then v=180 end
-        alpha=v*math.pi/180
+    else
+        v=0
     end
+    alpha=v*math.pi/180
     simUI.setEditValue(ui,4,string.format("%.1f",alpha*180/math.pi))
 end
 
@@ -117,9 +121,7 @@ function sysCall_nonSimulation()
     if leaveNow then
         return {cmd='cleanup'}
     end
-    local s=sim.getObjectSelection()
-    local show=(s and #s==1 )
-    if show then
+    if #sim.getObjectSel()==1 then
         showDlg()
     else
         hideDlg()
@@ -127,15 +129,13 @@ function sysCall_nonSimulation()
 end
 
 function buildJoint(revoluteJoint)
-    local selectedObjects=sim.getObjectSelection()
-    local dhParams={d,theta,a,alpha}
-    
-    local objMatr=sim.getObjectMatrix(selectedObjects[1],-1)
-    if sim.getObjectType(selectedObjects[1])==sim.object_joint_type then
-        objMatr=sim.multiplyMatrices(objMatr,sim.getJointMatrix(selectedObjects[1])) -- don't forget the joint's intrinsic transformation
+    local sel=sim.getObjectSel()
+    local objMatr=sim.getObjectMatrix(sel[1],-1)
+    if sim.getObjectType(sel[1])==sim.object_joint_type then
+        objMatr=sim.multiplyMatrices(objMatr,sim.poseToMatrix(sim.getObjectChildPose(sel[1]))) -- don't forget the joint's intrinsic transformation
     end
-    local m1=sim.buildMatrix({0,0,dhParams[1]},{0,0,dhParams[2]})
-    local m2=sim.buildMatrix({dhParams[3],0,0},{dhParams[4],0,0})
+    local m1=sim.buildMatrix({0,0,d},{0,0,theta})
+    local m2=sim.buildMatrix({r,0,0},{alpha,0,0})
     local m=sim.multiplyMatrices(m1,m2)
     objMatr=sim.multiplyMatrices(objMatr,m)
     local newJoint=-1
@@ -145,8 +145,7 @@ function buildJoint(revoluteJoint)
         newJoint=sim.createJoint(sim.joint_prismatic_subtype,sim.jointmode_force,0)
     end
     sim.setObjectMatrix(newJoint,-1,objMatr)
-    sim.setObjectParent(newJoint,selectedObjects[1],true)
-    sim.removeObjectFromSelection(sim.handle_all,-1)
-    sim.addObjectToSelection(sim.handle_single,newJoint)
+    sim.setObjectParent(newJoint,sel[1],true)
+    sim.setObjectSel({newJoint})
     sim.announceSceneContentChange()
 end
--- simAddOnMesh subdivision.lua
+++ simAddOnMesh subdivision.lua
@@ -1,6 +1,6 @@
 function sysCall_init()
     if sim.isPluginLoaded('IGL') then
-        local sel=sim.getObjectSelection()
+        local sel=sim.getObjectSel()
         if #sel~=1 or sim.getObjectType(sel[1])~=sim.object_shape_type then
             simUI.msgBox(simUI.msgbox_type.critical,simUI.msgbox_buttons.ok,'Mesh subdivision add-on','This tool requires exactly one shape to be selected.')
         else
