3f26791fc8b707aa5570dbcba92b66c70a139dca
Coppelia <mfreese@gmx.ch>
2023-04-11 09:01:46 +0200

    improved color changer

--- simAddOnColor changer.lua
+++ simAddOnColor changer.lua
@@ -12,13 +12,33 @@ function sysCall_addOnScriptSuspend()
     return {cmd='cleanup'}
 end
 
+function getSameColor(allCols,col)
+    for name,coll in pairs(allCols) do
+        local same=true
+        for i=1,3,1 do
+            for j=1,3,1 do
+                if coll[i][j]~=col[i][j] then
+                    same=false
+                    break
+                end
+            end
+            if not same then
+                break
+            end
+        end
+        if same and name:sub(1,1)=='*' then
+            return name
+        end
+    end
+end
+
 function sysCall_nonSimulation()
     if leaveNow then
         return {cmd='cleanup'}
     end
     selectedObjects=sim.getObjectSelection()
     if not selectedObjects then selectedObjects={} end
-    selectedObjects=getAlsoModelObjectsAndOnlyShapes(selectedObjects)
+    selectedObjects=getAlsoModelObjectsAndOnlyVisibleShapes(selectedObjects)
     if not areObjectsSame(selectedObjects,previousSelectedObjects) then
         hideDlg2()
         hideDlg1()
@@ -27,6 +47,47 @@ function sysCall_nonSimulation()
             previousSelectedObjects[i]=selectedObjects[i]
         end
         if #selectedObjects>0 then
+            allCols={}
+            allShapesAndCols={}
+            for i=1,#selectedObjects,1 do
+                local s=sim.getObjectStringParam(selectedObjects[i],sim.shapestringparam_colorname)
+                local r,a_cols=sim.getShapeColor(selectedObjects[i],'@compound',sim.colorcomponent_ambient_diffuse)
+                local r,b_cols=sim.getShapeColor(selectedObjects[i],'@compound',sim.colorcomponent_specular)
+                local r,c_cols=sim.getShapeColor(selectedObjects[i],'@compound',sim.colorcomponent_emission)
+                local scolnms={}
+                if s and s~='' then
+                    local i=0
+                    for token in string.gmatch(s,"[^%s]+") do
+                        local col={{a_cols[3*i+1],a_cols[3*i+2],a_cols[3*i+3]},{b_cols[3*i+1],b_cols[3*i+2],b_cols[3*i+3]},{c_cols[3*i+1],c_cols[3*i+2],c_cols[3*i+3]}}
+                        if token~='*' then
+                            colName=token
+                            allCols[colName]=col
+                        else
+                            colName=getSameColor(allCols,col)
+                            if not colName then
+                                local j=1
+                                while allCols['*'..j]~=nil do
+                                    j=j+1
+                                end
+                                colName='*'..j
+                                allCols[colName]=col
+                            end
+                        end
+                        scolnms[i+1]=colName
+                        i=i+1
+                    end
+                end
+                allShapesAndCols[#allShapesAndCols+1]=scolnms
+            end
+            colorNameTable={}
+            for name,coll in pairs(allCols) do
+                colorNameTable[#colorNameTable+1]=name
+            end
+            table.sort(colorNameTable)
+--            print(allCols)
+--            print(allShapesAndCols)
+--            print(colorNameTable)
+--[[
             colorNames={}
             for i=1,#selectedObjects,1 do
                 local s=sim.getObjectStringParam(selectedObjects[i],sim.shapestringparam_color_name)
@@ -40,6 +101,7 @@ function sysCall_nonSimulation()
             for k, v in pairs(colorNames) do
                 colorNameTable[#colorNameTable+1]=k
             end
+            --]]
             showDlg1(colorNameTable)
         end
     end
@@ -70,7 +132,9 @@ function showDlg1()
         end
         local xml ='<ui title="Color names" activate="false" closeable="true" on-close="close_callback" layout="vbox" '..pos..'>'
         for i=1,#colorNameTable,1 do
-            xml=xml..'<button text="'..colorNameTable[i]..'" on-click="colorClick_callback" id="'..i..'" />'
+            local cn=colorNameTable[i]
+            cn=cn:gsub("*","Unnamed color ")
+            xml=xml..'<button text="'..cn..'" on-click="colorClick_callback" id="'..i..'" />'
         end
         xml=xml..'</ui>'
         ui=simUI.create(xml)
@@ -101,7 +165,9 @@ function showDlg2()
     if ui2Pos then
         pos='position="'..ui2Pos[1]..','..ui2Pos[2]..'" placement="absolute"'
     end
-    local xml ='<ui title="Color ['..colorNameTable[colorNameIndex]..']" activate="false" closeable="true" on-close="close2_callback" layout="vbox" '..pos..'>'
+    local cn=colorNameTable[colorNameIndex]
+    cn=cn:gsub("*","Unnamed color ")
+    local xml ='<ui title="Color ['..cn..']" activate="false" closeable="true" on-close="close2_callback" layout="vbox" '..pos..'>'
     xml=xml..[[
         <label text="Ambient+diffuse" style="* {font-weight: bold;}"/>
             <group layout="form" flat="true">
@@ -134,7 +200,9 @@ function showDlg2()
             </group>
     </ui>]]
     ui2=simUI.create(xml)
-    ambientDiffuse,specular,emission=getColorValuesForColorName(colorNameTable[colorNameIndex],selectedObjects)
+    ambientDiffuse=allCols[colorNameTable[colorNameIndex]][1]
+    specular=allCols[colorNameTable[colorNameIndex]][2]
+    emission=allCols[colorNameTable[colorNameIndex]][3]
     for i=1,3,1 do
         simUI.setSliderValue(ui2,i+0,ambientDiffuse[i]*100)
         simUI.setSliderValue(ui2,i+3,specular[i]*100)
@@ -168,9 +236,21 @@ function sliderMoved(ui,id,v)
     end
     local colName=colorNameTable[colorNameIndex]
     for i=1,#selectedObjects,1 do
-        sim.setShapeColor(selectedObjects[i],colName,sim.colorcomponent_ambient_diffuse,ambientDiffuse)
-        sim.setShapeColor(selectedObjects[i],colName,sim.colorcomponent_specular,specular)
-        sim.setShapeColor(selectedObjects[i],colName,sim.colorcomponent_emission,emission)
+        local r,a_cols=sim.getShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_ambient_diffuse)
+        local r,b_cols=sim.getShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_specular)
+        local r,c_cols=sim.getShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_emission)
+        for j=1,#allShapesAndCols[i],1 do
+            if allShapesAndCols[i][j]==colName then
+                for k=1,3,1 do
+                    a_cols[3*(j-1)+k]=ambientDiffuse[k]
+                    b_cols[3*(j-1)+k]=specular[k]
+                    c_cols[3*(j-1)+k]=emission[k]
+                end
+            end
+        end
+        sim.setShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_ambient_diffuse,a_cols)
+        sim.setShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_specular,b_cols)
+        sim.setShapeColor(selectedObjects[i],"@compound",sim.colorcomponent_emission,c_cols)
     end
     sim.announceSceneContentChange()
 end
@@ -179,8 +259,8 @@ function close2_callback()
     hideDlg2()
 end
 
-getAlsoModelObjectsAndOnlyShapes=function(sel)
-    local retSel={}
+function getAlsoModelObjectsAndOnlyVisibleShapes(sel)
+    local tsel={}
     for i=1,#sel,1 do
         local p=sim.getModelProperty(sel[i])
         if (p&sim.modelproperty_not_model)==0 then
@@ -188,14 +268,14 @@ getAlsoModelObjectsAndOnlyShapes=function(sel)
             local modObjs=sim.getObjectsInTree(sel[i],sim.object_shape_type)
             for k=1,#modObjs,1 do
                 local addIt=true
-                for j=1,#retSel,1 do
-                    if retSel[j]==modObjs[k] then
+                for j=1,#tsel,1 do
+                    if tsel[j]==modObjs[k] then
                         addIt=false
                         break
                     end
                 end
                 if addIt then
-                    retSel[#retSel+1]=modObjs[k]
+                    tsel[#tsel+1]=modObjs[k]
                 end
             end
         else
@@ -203,22 +283,28 @@ getAlsoModelObjectsAndOnlyShapes=function(sel)
             if sim.getObjectType(sel[i])==sim.object_shape_type then
                 -- We have a shape
                 local addIt=true
-                for j=1,#retSel,1 do
-                    if retSel[j]==sel[i] then
+                for j=1,#tsel,1 do
+                    if tsel[j]==sel[i] then
                         addIt=false
                         break
                     end
                 end
                 if addIt then
-                    retSel[#retSel+1]=sel[i]
+                    tsel[#tsel+1]=sel[i]
                 end
             end
         end
     end
-    return retSel
+    sel={}
+    for i=1,#tsel,1 do
+        if sim.getObjectInt32Param(tsel[i],sim.objintparam_visible)~=0 then
+            sel[#sel+1]=tsel[i]
+        end
+    end
+    return sel
 end
 
-areObjectsSame=function(sel1,sel2)
+function areObjectsSame(sel1,sel2)
     if #sel1~=#sel2 then
         return false
     else
@@ -231,7 +317,7 @@ areObjectsSame=function(sel1,sel2)
     end
 end
 
-getColorValuesForColorName=function(colName,selObjects)
+function getColorValuesForColorName(colName,selObjects)
     for i=1,#selObjects,1 do
         local r,v0=sim.getShapeColor(selObjects[i],colName,sim.colorcomponent_ambient_diffuse)
         if r>0 then
