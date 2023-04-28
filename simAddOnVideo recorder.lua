27ce3ae07a64bee9dc7a4673afacdfc74b413d2f
Federico Ferri <federico.ferri.it@gmail.com>
2023-04-28 15:28:41 +0200

    add video encode options and command

--- simAddOnVideo recorder.lua
+++ simAddOnVideo recorder.lua
@@ -7,12 +7,23 @@ function sysCall_init()
     numCapturedFrames=0
     outputDir=sim.getStringParam(sim.stringparam_importexportdir)
     ui=simUI.create[[<ui title="Video Recorder" closeable="true" on-close="onUiClose">
-        <group layout="hbox" content-margins="0,0,0,0">
+        <group flat="true" layout="hbox" content-margins="0,0,0,0">
             <label text="Output dir:" />
             <edit id="${editOutputBaseDir}" value="${outputDir}" />
             <button id="${btnBrowseOutputDir}" text="Browse..." on-click="browse" />
         </group>
-        <group layout="hbox" content-margins="0,0,0,0">
+        <checkbox id="${chkMakeVideo}" text="Convert image sequence to video" on-change="updateUi" />
+        <group enabled="false" id="${grpVideoEncoderOptions}" flat="true" layout="vbox" content-margins="0,0,0,0">
+            <group flat="true" layout="hbox" content-margins="0,0,0,0">
+                <label text="Frames per second:" />
+                <edit id="${editFPS}" value="20" />
+            </group>
+            <group flat="true" layout="hbox" content-margins="0,0,0,0">
+                <label text="Bitrate: (kbps)" />
+                <edit id="${editBitrate}" value="1800" />
+            </group>
+        </group>
+        <group flat="true" layout="hbox" content-margins="0,0,0,0">
             <button id="${btnStartCapture}" text="Start capture" on-click="startCapture" />
             <button id="${btnStopCapture}" text="Stop capture" on-click="stopCapture" enabled="false" />
         </group>
@@ -48,6 +59,7 @@ function sysCall_nonSimulation()
 end
 
 function onUiClose()
+    leaveNow=true
 end
 
 function browse()
@@ -60,7 +72,7 @@ end
 function startCapture()
     if capturing then return end
     capturing=true
-    filePrefix=string.format('%d_',os.time())
+    filePrefix=string.format('%d',os.time())
     numCapturedFrames=0
     updateUi()
 end
@@ -69,6 +81,13 @@ function stopCapture()
     if not capturing then return end
     capturing=false
     if numCapturedFrames>0 then
+        if simUI.getCheckboxValue(ui,chkMakeVideo)>0 then
+            local fps=tonumber(simUI.getEditValue(ui,editFPS))
+            local br=tonumber(simUI.getEditValue(ui,editBitrate))
+            local inf=string.format('%s/%s_%s.png',outputDir,filePrefix,'%08d')
+            local outf=string.format('%s/%s.mp4',outputDir,filePrefix)
+            sim.addLog(sim.verbosity_scriptinfos,string.format('will run: ffmpeg -r %d -i \"%s\" -c:v libx264 -b:v %dk \"%s\"',fps,inf,br,outf))
+        end
     end
     updateUi()
 end
@@ -77,11 +96,12 @@ function updateUi()
     simUI.setEnabled(ui,btnStartCapture,not capturing)
     simUI.setEnabled(ui,btnStopCapture,capturing)
     simUI.setEditValue(ui,editOutputBaseDir,outputDir)
+    simUI.setEnabled(ui,grpVideoEncoderOptions,simUI.getCheckboxValue(ui,chkMakeVideo)>0)
 end
 
 function capture()
     local img,res=sim.getScaledImage('\xff\x00\xff',{1,1},{1024,768},0)
-    local fileName=string.format('%s/%s%08d.png',outputDir,filePrefix,numCapturedFrames)
+    local fileName=string.format('%s/%s_%08d.png',outputDir,filePrefix,numCapturedFrames)
     numCapturedFrames=numCapturedFrames+1
     sim.saveImage(img,res,0,fileName,-1)
 end
