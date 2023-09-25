741a309af1e10d7d85d9ef8d8017cc5db88a3cf5
Federico Ferri <federico.ferri.it@gmail.com>
2023-09-25 14:31:35 +0200

    support for joint groups

--- simAddOnJoint tool.lua
+++ simAddOnJoint tool.lua
@@ -10,7 +10,7 @@ end
 
 function sysCall_init()
     simUI=require'simUI'
-    sim.addLog(sim.verbosity_scriptinfos,"Select an object to use the Joint tool.")
+    sim.addLog(sim.verbosity_scriptinfos,"Select a model, a joint group, or individual joints to use the Joint tool.")
     sel={}
     idToJointMap={}
     jointToIdMap={}
@@ -36,30 +36,51 @@ function closeUi_user()
 end
 
 function onSelectionChanged()
-    aliasOption=sim.getNamedInt32Param('addons.jointTool.aliasOption') or -1
+    -- get selected joints:
+    local jointHandles={}
+    for i,objectHandle in ipairs(sel) do
+        if sim.readCustomDataBlock(objectHandle,'__jointGroup__') then
+            for j,jointHandle in ipairs(sim.getReferencedHandles(objectHandle)) do
+                if sim.getObjectType(jointHandle)==sim.object_joint_type then
+                    table.insert(jointHandles,jointHandle)
+                else
+                    sim.addLog(sim.verbosity_warnings,'ignoring object referenced by joint group '..sim.getObjectAlias(objectHandle,2)..': '..sim.getObjectAlias(jointHandle,2))
+                end
+            end
+        elseif sim.getModelProperty(objectHandle)&sim.modelproperty_not_model==0 then
+            for j,jointHandle in ipairs(sim.getObjectsInTree(objectHandle,sim.object_joint_type)) do
+                table.insert(jointHandles,jointHandle)
+            end
+        elseif sim.getObjectType(objectHandle)==sim.object_joint_type then
+            table.insert(jointHandles,objectHandle)
+        else
+            sim.addLog(sim.verbosity_warnings,'ignoring selected object: '..sim.getObjectAlias(objectHandle,2))
+        end
+    end
+
     idToJointMap={}
     jointToIdMap={}
     local nid=1
     closeUi()
-    for i,sh in ipairs(sel) do
-        for j,h in ipairs(sim.getObjectsInTree(sh,sim.object_joint_type)) do
-            local isSpherical=sim.getJointType(h)==sim.joint_spherical_subtype
-            local mh,a,b=sim.getJointDependency(h)
-            if not isSpherical and mh==-1 and not jointToIdMap[h] then
-                jointToIdMap[h]=nid
-                idToJointMap[nid]=h
-                nid=nid+1
-            end
+    for i,jointHandle in ipairs(jointHandles) do
+        local isSpherical=sim.getJointType(jointHandle)==sim.joint_spherical_subtype
+        local mh,a,b=sim.getJointDependency(jointHandle)
+        if not isSpherical and mh==-1 and not jointToIdMap[jointHandle] then
+            jointToIdMap[jointHandle]=nid
+            idToJointMap[nid]=jointHandle
+            nid=nid+1
         end
     end
     if nid==1 then return end
+
+    aliasOption=sim.getNamedInt32Param('addons.jointTool.aliasOption') or -1
     local uiPosStr=uiPos and string.format('placement="absolute" position="%d,%d"' ,table.unpack(uiPos)) or 'placement="relative" position="280,500" '
     xml='<ui closeable="true" '..uiPosStr..'resizable="false" on-close="closeUi_user" title="Joint tool" layout="form">\n'
-    for id,h in pairs(idToJointMap) do
-        local v=sim.getJointPosition(h)*180/math.pi
-        local cyclic,i=sim.getJointInterval(h)
+    for id,jointHandle in pairs(idToJointMap) do
+        local v=sim.getJointPosition(jointHandle)*180/math.pi
+        local cyclic,i=sim.getJointInterval(jointHandle)
         local vmin,vmax=i[1]*180/math.pi,(i[1]+i[2])*180/math.pi
-        xml=xml..string.format('    <label text="%s" />\n',sim.getObjectAlias(h,aliasOption))
+        xml=xml..string.format('    <label text="%s" />\n',sim.getObjectAlias(jointHandle,aliasOption))
         xml=xml..string.format('    <group flat="true" content-margins="0,0,0,0" layout="hbox"><spinbox id="%s" value="%f" minimum="%f" maximum="%f" step="0.5" on-change="setJointPos" /><label text="%.1f~%.1f [deg]" enabled="false" /></group>\n',id,v,vmin,vmax,vmin,vmax)
     end
     xml=xml..'</ui>'
