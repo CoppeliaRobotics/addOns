7edecb88871669d02a0a73971f1caa585bf0cb30
Federico Ferri <federico.ferri.it@gmail.com>
2023-11-16 16:07:17 +0100

    add more dummy creation options

--- simAddOnCreate dummies on mesh surface.lua
+++ simAddOnCreate dummies on mesh surface.lua
@@ -10,6 +10,10 @@ end
 
 function sysCall_init()
     simUI = require 'simUI'
+    dummySize = 0.02
+    dummyColor = {0, 1, 0}
+    alias = 'Dummy'
+    zOffset = 0
     sim.addLog(
         sim.verbosity_scriptinfos,
         "This tool allows to create dummies from points sampled on mesh surfaces"
@@ -62,28 +66,28 @@ function clearTextInfo()
 end
 
 function createDummy(m)
-    local h = sim.createDummy(0.02)
-    sim.setObjectColor(h, 0, sim.colorcomponent_ambient_diffuse, {0, 1, 0})
+    local h = sim.createDummy(dummySize)
+    sim.setObjectColor(h, 0, sim.colorcomponent_ambient_diffuse, dummyColor)
     sim.setObjectMatrix(h, m, sim.handle_world)
-    local zOffset = simUI.getSpinboxValue(ui, 8)
     sim.setObjectPose(h, {0, 0, zOffset, 0, 0, 0, 1}, h)
-    local alias = simUI.getEditValue(ui, 6)
     sim.setObjectAlias(h, alias)
 end
 
 function showDlg()
     if not ui then
-        local pos = 'position="-50,50" placement="relative"'
+        local pos = 'position="-50,150" placement="relative"'
         if uiPos then
             pos = 'position="' .. uiPos[1] .. ',' .. uiPos[2] .. '" placement="absolute"'
         end
-        local xml =
-            '<ui title="Create dummies on mesh surface" style="min-width: 9em;" activate="false" closeable="true" resizable="true" on-close="close_callback" ' ..
-                pos .. [[>
-            <label id="5" text="Alias:"/>
-            <edit id="6" value="Dummy"/>
-            <label id="7" text="Offset: [m]"/>
-            <spinbox id="8" value="0.0" step="0.01"/>
+        local xml = [[<ui title="Create dummies on mesh surface" layout="form" activate="false" closeable="true" resizable="true" on-close="close_callback" ]] ..  pos .. [[>
+            <label id="11" text="Size:" />
+            <spinbox id="12" value="]] .. dummySize .. [[" step="0.001" decimals="3" suffix="m" on-change="changedDummySize" />
+            <label id="13" text="Color:" />
+            <edit id="14" value="]] .. table.join(dummyColor) .. [[" on-editing-finished="changedColor" />
+            <label id="5" text="Alias:" />
+            <edit id="6" value="]] .. alias .. [[" on-editing-finished="changedAlias" />
+            <label id="7" text="Offset:" />
+            <spinbox id="8" value="]] .. zOffset .. [[" step="0.001" suffix="m" on-change="changedZOffset" />
         </ui>]]
         ui = simUI.create(xml)
     end
@@ -96,6 +100,35 @@ function hideDlg()
         simUI.destroy(ui)
         ui = nil
     end
+end
+
+function changedDummySize(ui, id, val)
+    dummySize = val
+end
+
+function changedColor(ui, id, val)
+    local s = string.split(simUI.getEditValue(ui, 14), ',')
+    if #s == 3 then
+        local valid = true
+        for i = 1, 3 do
+            s[i] = tonumber(s[i])
+            if s[i] == nil then
+                valid = false
+            end
+        end
+        if valid then
+            dummyColor = s
+        end
+    end
+    simUI.setEditValue(ui, 14, table.join(dummyColor))
+end
+
+function changedAlias(ui, id, val)
+    alias = val
+end
+
+function changedZOffset(ui, id, val)
+    zOffset = val
 end
 
 function close_callback()
