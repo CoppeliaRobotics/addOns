cfb98d9fe3427eee282bfa5b198ed1a3e09c7f2e
Federico Ferri <federico.ferri.it@gmail.com>
2025-04-10 14:44:07 +0200

    start from modelPath if scenePath empty or shift is pressed

--- External model load.lua
+++ External model load.lua
@@ -7,7 +7,13 @@ function sysCall_init()
     local simUI = require 'simUI'
     local extModel = require 'addOns.extModel'
     local lfsx = require 'lfsx'
-    local initPath = lfsx.dirname(sim.getStringProperty(sim.handle_scene, 'scenePath'))
+    local initPath = ''
+    local scenePath = sim.getStringProperty(sim.handle_scene, 'scenePath')
+    if scenePath ~= '' and not simUI.getKeyboardModifiers().shift then
+        initPath = lfsx.dirname(scenePath)
+    else
+        initPath = sim.getStringProperty(sim.handle_app, 'modelPath')
+    end
     files = simUI.fileDialog(simUI.filedialog_type.load, 'Open model...', initPath, '', 'Model files', 'ttm;simmodel.xml')
     for _, file in ipairs(files) do
         extModel.loadModel(nil, file)
