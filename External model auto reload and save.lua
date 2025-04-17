98d187168f28b6c783a179fb954a82ae98f9bffc
Federico Ferri <federico.ferri.it@gmail.com>
2025-04-17 10:58:24 +0200

    use addOns.autoStart lib

--- External model auto reload and save.lua
+++ External model auto reload and save.lua
@@ -3,18 +3,15 @@ extModel = require 'addOns.extModel'
 
 function sysCall_info()
     return {
-        autoStart = sim.getBoolProperty(sim.handle_app, 'customData.extModel.autoStart', {noError = true}) == true,
         menu = 'Developer tools\nExternal model auto reload and save',
     }
 end
 
 function sysCall_addOnScriptSuspend()
-    sim.setBoolProperty(sim.handle_app, 'customData.extModel.autoStart', false)
     return {cmd = 'cleanup'}
 end
 
 function sysCall_init()
-    sim.setBoolProperty(sim.handle_app, 'customData.extModel.autoStart', true)
 end
 
 function sysCall_afterLoad()
@@ -30,3 +27,5 @@ function sysCall_beforeSave(inData)
         extModel.scanForExtModelsToSave()
     end
 end
+
+require('addOns.autoStart').setup{ns = 'extModel', readNamedParam = false}
