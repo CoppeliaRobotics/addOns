979d5659d5bfe97bd37e7b9454ff22c3a17daf00
Federico Ferri <federico.ferri.it@gmail.com>
2025-04-25 15:01:24 +0200

    cannot be used if scene is not saved to file

--- External model load.lua
+++ External model load.lua
@@ -5,14 +5,18 @@ end
 function sysCall_init()
     local sim = require 'sim'
     local simUI = require 'simUI'
+    local scenePath = sim.getStringProperty(sim.handle_scene, 'scenePath')
+    if scenePath == '' then
+        simUI.msgBox(simUI.msgbox_type.critical, simUI.msgbox_buttons.ok, 'Error', 'To use this add-on, first save the scene to a file.')
+        return {cmd = 'cleanup'}
+    end
     local extModel = require 'addOns.extModel'
     local lfsx = require 'lfsx'
     local initPath = ''
-    local scenePath = sim.getStringProperty(sim.handle_scene, 'scenePath')
-    if scenePath ~= '' and not simUI.getKeyboardModifiers().shift then
-        initPath = lfsx.dirname(scenePath)
-    else
+    if simUI.getKeyboardModifiers().shift then
         initPath = sim.getStringProperty(sim.handle_app, 'modelPath')
+    else
+        initPath = lfsx.dirname(scenePath)
     end
     files = simUI.fileDialog(simUI.filedialog_type.load, 'Open model...', initPath, '', 'Model files', 'ttm;simmodel.xml')
     for _, file in ipairs(files) do
