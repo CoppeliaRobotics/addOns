c74472d1ff048b2c956e0074e166f0862c269197
Federico Ferri <federico.ferri.it@gmail.com>
2025-05-15 11:23:03 +0200

    some improvements to Event Viewer addon

--- Event viewer.lua
+++ Event viewer.lua
@@ -25,9 +25,9 @@ end
 function sysCall_event(event)
     local txt = ''
     for _, e in ipairs(cbor.decode(tostring(event))) do
-        if testFilter(e) then
-            local s = _S.tableToString(e, {indent = true}, 99)
-            txt = txt .. (sep or '') .. s .. ','
+        local ret = processEvent(e)
+        if ret then
+            txt = txt .. (sep or '') .. ret .. ','
             sep = '\n\n'
         end
     end
@@ -42,6 +42,23 @@ function sysCall_sensing()
     if leaveNow then return {cmd = 'cleanup'} end
 end
 
+function processEvent(e)
+    local function p(n)
+        return sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.' .. n, {noError = true})
+    end
+    if p('excludeLogMsg') and e.event == 'logMsg' then return end
+    if p('excludeMsgDispatchTime') and e.event == 'msgDispatchTime' then return end
+    local ks = table.keys(e.data)
+    table.sort(ks)
+    if p('excludeSelectionEvents') and e.event == 'objectChanged' and e.handle == sim.handle_scene and table.eq(ks, {'selectionHandles'}) then return end
+    if p('excludeSelectionEvents') and e.event == 'objectChanged' and e.handle ~= sim.handle_scene and table.eq(ks, {'selected'}) then return end
+    if p('excludeCollapseEvents') and e.event == 'objectChanged' and table.eq(ks, {'collapsed', 'objectPropertyFlags'}) then return end
+
+    if not testFilter(e) then return end
+
+    return _S.tableToString(e, {indent = true}, 99)
+end
+
 function testFilter(e)
     if not filterEnabled then return true end
     if not filterFunc then return false end
@@ -51,14 +68,31 @@ function testFilter(e)
 end
 
 function onFilterChanged()
-    filterEnabled = simUI.getCheckboxValue(ui, ui_chkFilter) > 0
-    simUI.setEnabled(ui, ui_txtFilter, filterEnabled)
+    for pname, ui_ctrl in pairs {
+        filterEnabled = ui_chkFilter,
+        excludeLogMsg = ui_chkExcludeLogMsg,
+        excludeMsgDispatchTime = ui_chkExcludeMsgDispatchTime,
+        excludeSelectionEvents = ui_chkExcludeSelectionEvents,
+        excludeCollapseEvents = ui_chkExcludeCollapseEvents,
+    } do
+        local checkboxValue = simUI.getCheckboxValue(ui, ui_ctrl) > 0
+        sim.setBoolProperty(sim.handle_app, 'customData.eventViewer.' .. pname, checkboxValue)
+    end
+
     local filterFuncStr = simUI.getEditValue(ui, ui_txtFilter)
     filterFunc = loadstring('return function(e) return ' .. filterFuncStr .. ' end')
     simUI.setStyleSheet(ui, ui_txtFilter, filterFunc and '' or 'border: 1px solid red')
-
-    sim.setBoolProperty(sim.handle_app, 'customData.eventViewer.filterEnabled', filterEnabled)
     sim.setStringProperty(sim.handle_app, 'customData.eventViewer.filterFunc', filterFuncStr)
+
+    filterEnabled = sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.filterEnabled')
+    simUI.setEnabled(ui, ui_txtFilter, filterEnabled)
+
+    simUI.setCheckboxValue(ui, ui_chkExclude, (
+            sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.excludeLogMsg')
+            or sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.excludeMsgDispatchTime')
+            or sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.excludeSelectionEvents')
+            or sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.excludeCollapseEvents')
+        ) and 2 or 0)
 end
 
 function onClose()
@@ -81,17 +115,35 @@ function createUi()
         end
 
         ui = simUI.create([[<ui title="Event viewer"]] .. xml_pos .. xml_sz .. [[ closeable="true" resizable="true" on-close="onClose">
-            <group layout="hbox">
+            <group layout="grid">
+                <checkbox id="${ui_chkExclude}" text="" checked="true" enabled="false" />
+                <label text="Exclude:" />
+                <group layout="hbox" content-margins="0,0,0,0" flat="true">
+                    <checkbox id="${ui_chkExcludeLogMsg}" text="logMsg" checked="true" on-change="onFilterChanged" />
+                    <checkbox id="${ui_chkExcludeMsgDispatchTime}" text="msgDispatchTime" checked="true" on-change="onFilterChanged" />
+                    <checkbox id="${ui_chkExcludeSelectionEvents}" text="selection events" checked="true" on-change="onFilterChanged" />
+                    <checkbox id="${ui_chkExcludeCollapseEvents}" text="hierarchy collapse events" checked="true" on-change="onFilterChanged" />
+                    <stretch />
+                </group>
+                <br/>
                 <checkbox id="${ui_chkFilter}" text="" on-change="onFilterChanged" />
                 <label text="Filter:" />
-                <edit id="${ui_txtFilter}" value="e.event ~= 'logMsg' and e.event ~= 'msgDispatchTime'" enabled="false" on-change="onFilterChanged" />
+                <edit id="${ui_txtFilter}" value="e.handle == sim.handle_scene" enabled="false" on-change="onFilterChanged" />
             </group>
             <text-browser id="${ui_txtLog}" type="plain" word-wrap="false" read-only="false" style="QTextBrowser { font-family: Courier New; }" />
         </ui>]])
 
-        local fe = sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.filterEnabled', {noError = true})
-        if fe ~= nil then
-            simUI.setCheckboxValue(ui, ui_chkFilter, fe and 2 or 0)
+        for pname, ui_ctrl in pairs {
+            filterEnabled = ui_chkFilter,
+            excludeLogMsg = ui_chkExcludeLogMsg,
+            excludeMsgDispatchTime = ui_chkExcludeMsgDispatchTime,
+            excludeSelectionEvents = ui_chkExcludeSelectionEvents,
+            excludeCollapseEvents = ui_chkExcludeCollapseEvents,
+        } do
+            local pvalue = sim.getBoolProperty(sim.handle_app, 'customData.eventViewer.' .. pname, {noError = true})
+            if pvalue ~= nil then
+                simUI.setCheckboxValue(ui, ui_ctrl, pvalue and 2 or 0)
+            end
         end
 
         local ff = sim.getStringProperty(sim.handle_app, 'customData.eventViewer.filterFunc', {noError = true})
